203f1fb6861dd9ab9db010144d5a59fc45f4a421
Rawllings Kiprop <rutojr01@gmail.com>
2024-07-30 12:54:58 +0300

    done with first fetch process

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,18 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  fetch("http://localhost:3000/places")
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData);
+    });
+
   return (
     <Places
       title="Available Places"
